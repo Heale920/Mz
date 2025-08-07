75cdc9718652fd88056e059edf82ab2cb6d89bce
江毅川 <2690477449@qq.com>
2025-08-07 20:09:19 +0800

    Fix: correct publicPath

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,26 +1,26 @@
-  const { defineConfig } = require('@vue/cli-service');
-  const { DefinePlugin } = require('webpack');
+const { defineConfig } = require('@vue/cli-service');
+const { DefinePlugin } = require('webpack');
 
-  module.exports = defineConfig({
-    devServer: {
-      port: 8082,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8081', // 后端服务器地址
-          changeOrigin: true,
-          pathRewrite: {
-            '^/api': '' // 移除/api前缀
-          }
+module.exports = defineConfig({
+  devServer: {
+    port: 8082,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081', // 后端服务器地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
         }
       }
-    },
-    transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/myapp/' : '/',
-    configureWebpack: {
-      plugins: [
-        new DefinePlugin({
-          __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
-        }),
-      ],
-    },
-  });
+    }
+  },
+  transpileDependencies: true,
+  publicPath: '/',
+  configureWebpack: {
+    plugins: [
+      new DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
+      }),
+    ],
+  },
+});
