bb4721085612848247dc46afa06672716eaa611d
江毅川 <2690477449@qq.com>
2025-08-08 12:15:56 +0800

    changes

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
     }
   },
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: './',
   configureWebpack: {
     plugins: [
       new DefinePlugin({
